b18099ccd7b8c01f2f54832288cd1800ae5f9097
student <student@158.197.35.96>
2019-09-17 09:37:20 +0200

    initial commit

---
+++ src/main/java/sk/hrehova/test_gitu/App.java
@@ -0,0 +1,13 @@
+package sk.hrehova.test_gitu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
