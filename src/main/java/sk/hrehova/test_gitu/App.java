b9ae0e0e120ee974694bbfbfd34e2124229cb7ad
student <student@158.197.35.96>
2019-09-17 11:08:18 +0200

    dzive

--- src/main/java/sk/hrehova/test_gitu/App.java
+++ src/main/java/sk/hrehova/test_gitu/App.java
@@ -8,12 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-
+<<<<<<< HEAD
         System.out.println( "Hello World" );
         System.out.println("Viky ma nezavolala na raňajky");
         
-
+=======
         System.out.println( "Hello Matka!" );
-
+>>>>>>> branch 'master' of https://github.com/hrehinka/git-test.git
     }
 }
