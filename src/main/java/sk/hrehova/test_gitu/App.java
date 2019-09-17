d293ab50756488a57ea0703ab9dd60ce03f5fd61
student <student@158.197.35.96>
2019-09-17 11:01:19 +0200

    takto

--- src/main/java/sk/hrehova/test_gitu/App.java
+++ src/main/java/sk/hrehova/test_gitu/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World" );
         System.out.println("Viky ma nezavolala na raňajky");
         
     }
