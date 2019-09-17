216f6de70c0939c571c50eeb7892d0d791507429
student <student@158.197.35.96>
2019-09-17 10:57:04 +0200

    odkaz pre kamku + nova

--- src/main/java/sk/hrehova/test_gitu/App.java
+++ src/main/java/sk/hrehova/test_gitu/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Viky ma nezavolala na raňajky");
+        
     }
 }
---
+++ src/main/java/sk/hrehova/test_gitu/Nova.java
@@ -0,0 +1,5 @@
+package sk.hrehova.test_gitu;
+
+public class Nova {
+
+}
